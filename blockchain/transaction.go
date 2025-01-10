1b576fc2290164b6637b064736ebba6ca2050300
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-10 22:05:30 +0800

    Add Timestamp field and IsValid method to Transaction struct

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,5 +4,10 @@ type Transaction struct {
 	Sender    string
 	Recipient string
 	Amount    float64
+	Timestamp string
 	Signature string
+}
+
+func (tx *Transaction) IsValid(publicKey string) bool {
+	return true
 }
