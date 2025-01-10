e49d32da909c592a1e539807e2ae45431a2f4f74
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-10 21:44:22 +0800

    Add blockchain package with Block struct and hash calculation methods

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
 type Block struct {
 	Index        int
 	Timestamp    string
@@ -7,4 +13,34 @@ type Block struct {
 	PrevHash     string
 	Hash         string
 	Nonce        int
+}
+
+func NewGenesisBlock() Block {
+	return Block{
+		Index:        0,
+		Timestamp:    "2021-01-01",
+		Transactions: []Transaction{},
+		PrevHash:     "",
+		Hash:         "",
+		Nonce:        0,
+	}
+}
+
+func NewBlock(index int, transactions []Transaction, prevHash string) Block {
+	block := Block{
+		Index:        index,
+		Timestamp:    "2021-01-01",
+		Transactions: transactions,
+		PrevHash:     prevHash,
+		Hash:         "",
+		Nonce:        0,
+	}
+	block.Hash = block.CalculateHash()
+	return block
+}
+
+func (b *Block) CalculateHash() string {
+	data := fmt.Sprintf("%d%s%s%d", b.Index, b.Timestamp, b.PrevHash, b.Nonce)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
 }
