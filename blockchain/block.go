7c0a5c9462641779cd39101f15435a75cb0ecb9a
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-12 17:35:42 +0800

    Refactor Block struct to use int64 for Index and Timestamp, and update NewGenesisBlock and NewBlock functions accordingly

--- blockchain/block.go
+++ blockchain/block.go
@@ -8,29 +8,31 @@ import (
 )
 
 type Block struct {
-	Index        int
-	Timestamp    string
-	Transactions []Transaction
-	PrevHash     string
-	Hash         string
-	Nonce        int
+	Index        int64         // Position in the chain
+	Timestamp    int64         // Unix timestamp
+	Transactions []Transaction // List of transactions
+	PrevHash     string        // Hash of the previous block
+	Hash         string        // Hash of the block
+	Nonce        int           // Proof of work
 }
 
+// NewGenesisBlock creates the first block in the blockchain
 func NewGenesisBlock() Block {
 	return Block{
 		Index:        0,
-		Timestamp:    "2021-01-01",
+		Timestamp:    time.Now().Unix(),
 		Transactions: []Transaction{},
 		PrevHash:     "",
-		Hash:         "",
+		Hash:         "genesis",
 		Nonce:        0,
 	}
 }
 
-func NewBlock(index int, transactions []Transaction, prevHash string) Block {
+// NewBlock creates a new block in the blockchain
+func NewBlock(index int64, transactions []Transaction, prevHash string) Block {
 	block := Block{
 		Index:        index,
-		Timestamp:    time.Now().String(),
+		Timestamp:    time.Now().Unix(),
 		Transactions: transactions,
 		PrevHash:     prevHash,
 		Hash:         "",
@@ -40,8 +42,9 @@ func NewBlock(index int, transactions []Transaction, prevHash string) Block {
 	return block
 }
 
+// CalculateHash calculates the hash of the block
 func (b *Block) CalculateHash() string {
-	data := fmt.Sprintf("%d%s%s%d", b.Index, b.Timestamp, b.PrevHash, b.Nonce)
+	data := fmt.Sprintf("%d%d%s%d", b.Index, b.Timestamp, b.PrevHash, b.Nonce)
 	hash := sha256.Sum256([]byte(data))
 	return hex.EncodeToString(hash[:])
 }
