d02972b4a73cebe6ae1a647105eccfc0b5137b5f
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-14 00:35:03 +0800

    Add message queue support to P2PNode and update Start method

--- p2p/node.go
+++ p2p/node.go
@@ -1,12 +1,17 @@
 package p2p
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
+)
 
 type P2PNode struct {
 	BitcoinAddress    string   // Bitcoin address of the node
 	Address           string   // IP address of the node
 	MemberList        []Member // List of members in the network
 	MemberListSelfPos int      // Position of the node in the member list
+	MsgQueue          []string // Message queue for the node
 }
 
 // NewP2PNode creates a new P2P node
@@ -20,10 +25,29 @@ func NewP2PNode(bitcoinAddress, address string) *P2PNode {
 }
 
 // Start starts the P2P node
-func (node *P2PNode) Start(bootstrapNodeAddress string) error {
+func (node *P2PNode) Start(port string, bootstrapNodeAddress string) error {
 	err := node.IntroduceSelfToGroup(bootstrapNodeAddress)
 	if err != nil {
 		return fmt.Errorf("failed to introduce self to group: %v", err)
 	}
+
+	err = network.StartListener(port, node.MsgQueue)
+	if err != nil {
+		return fmt.Errorf("failed to start listener: %v", err)
+	}
+
 	return nil
+}
+
+func (node *P2PNode) HandleMessageFromQueue() {
+	if len(node.MsgQueue) == 0 {
+		return
+	}
+
+	// Pop the first message from the queue
+	// message := node.MsgQueue[0]
+	// node.MsgQueue = node.MsgQueue[1:]
+
+	// Handle the message
+
 }
