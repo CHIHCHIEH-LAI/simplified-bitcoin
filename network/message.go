123a5955502ce49ecf73027b0f1228d33f96bbfa
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-14 00:00:47 +0800

    Add Message struct with serialization and deserialization methods

--- network/message.go
+++ network/message.go
@@ -6,6 +6,37 @@ import (
 	"net"
 )
 
+type Message struct {
+	Type    string `json:"type"`    // Type of the message (e.g. HEARTBEAT, TRANSACTION, BLOCK, etc)
+	Sender  string `json:"sender"`  // Sender of the message
+	Payload string `json:"payload"` // Payload of the message (as JSON string)
+}
+
+// NewMessage creates a new message
+func NewMessage(messageType, sender, payload string) *Message {
+	return &Message{
+		Type:    messageType,
+		Sender:  sender,
+		Payload: payload,
+	}
+}
+
+// Serialize serializes the message into a string
+func (m *Message) Serialize() string {
+	return fmt.Sprintf("%s|%s|%s", m.Type, m.Sender, m.Payload)
+}
+
+// DeserializeMessage deserializes the message from a string
+func DeserializeMessage(data string) (*Message, error) {
+	var msg Message
+	_, err := fmt.Sscanf(data, "%s|%s|%s", &msg.Type, &msg.Sender, &msg.Payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to deserialize message: %v", err)
+	}
+
+	return &msg, nil
+}
+
 // SendMessage sends a message to the specified address
 func SendMessage(address string, message string) error {
 	// Establish a connection to the remote address
