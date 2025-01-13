4da2d8c58b6c0bff9c0c444c35a655bb614ffc8b
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-13 21:56:03 +0800

    Add TCP server listener and message sender functions

---
+++ network/network.go
@@ -0,0 +1,49 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+// StartListener starts a TCP server and listens for incoming connections
+func StartListener(port string, handleConnection func(net.Conn)) error {
+	// Start listening on the specified port
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return fmt.Errorf("failed to start server on port %s: %v", port, err)
+	}
+	defer listener.Close()
+	log.Printf("Server started on port %s\n", port)
+
+	for {
+		// Accept a single connection
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %v\n", err)
+			continue
+		}
+
+		// Handle the connection (blocking call)
+		handleConnection(conn)
+	}
+}
+
+// SendMessage sends a message to the specified address
+func SendMessage(address string, message string) error {
+	// Establish a connection to the remote address
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to connect to %s: %v", address, err)
+	}
+	defer conn.Close()
+
+	// Send the message
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return fmt.Errorf("failed to send message to %s: %v", address, err)
+	}
+
+	log.Printf("Message sent to %s: %s\n", address, message)
+	return nil
+}
