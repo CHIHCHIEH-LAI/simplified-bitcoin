570ce0254fc47875ecc5c4cea133ee699836064f
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-13 22:24:08 +0800

    Refactor StartListener to remove handleConnection parameter and add handleConnection function for incoming connections

--- network/network.go
+++ network/network.go
@@ -7,7 +7,7 @@ import (
 )
 
 // StartListener starts a TCP server and listens for incoming connections
-func StartListener(port string, handleConnection func(net.Conn)) error {
+func StartListener(port string) error {
 	// Start listening on the specified port
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
@@ -24,9 +24,27 @@ func StartListener(port string, handleConnection func(net.Conn)) error {
 			continue
 		}
 
+		// TODO: Handle the connection in a separate goroutine
 		// Handle the connection (blocking call)
 		handleConnection(conn)
 	}
+}
+
+// handleConnection handles incoming connections
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Read data from the connection
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Failed to read from connection: %v\n", err)
+		return
+	}
+
+	// Print the received message
+	message := string(buffer[:n])
+	log.Printf("Received message: %s\n", message)
 }
 
 // SendMessage sends a message to the specified address
