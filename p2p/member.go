543140aa13287cba6671123fe701e8c8655436c8
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-14 23:28:30 +0800

    Remove BitcoinAddress field from Member struct for simplification

--- p2p/member.go
+++ p2p/member.go
@@ -1,8 +1,7 @@
 package p2p
 
 type Member struct {
-	BitcoinAddress string
-	Address        string
-	Heartbeat      int64
-	Timestamp      int64
+	Address   string
+	Heartbeat int64
+	Timestamp int64
 }
