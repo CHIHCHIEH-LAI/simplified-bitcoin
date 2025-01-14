1915ccc77ababd297346635334e56e667748e6ce
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-14 23:28:35 +0800

    Implement HandleMessage method for processing incoming messages in Node

--- p2p/node.go
+++ p2p/node.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"log"
 
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
 )
 
@@ -41,7 +42,39 @@ func (node *Node) Run(port string, bootstrapNodeAddress string) error {
 	}
 
 	// Start processing messages
-	// node.HandleMessage()
+	go node.HandleMessage()
 
 	return nil
+}
+
+// HandleMessage processes incoming messages
+func (node *Node) HandleMessage() {
+	for msgData := range node.MessageChannel {
+		log.Printf("Received message: %s\n", msgData)
+
+		// Parse the message
+		msg, err := message.DeserializeMessage(msgData)
+		if err != nil {
+			log.Printf("Failed to deserialize message: %v\n", err)
+			continue
+		}
+
+		// Process the message based on its type
+		switch {
+		// case msg.Type == "JOINREQ":
+		// 	node.HandleJoinRequest(msg)
+		// case msg.Type == "JOINRESP":
+		// 	node.HandleJoinResponse(msg)
+		// case msg.Type == "HEARTBEAT":
+		// 	node.HandleHeartbeat(msg)
+		// case msg.Type == "NEWBLOCK":
+		// 	node.HandleNewBlock(msg)
+		// case msg.Type == "NEWTRANSACTION":
+		// 	node.HandleNewTransaction(msg)
+		// case msg.Type == "GETBLOCKCHAIN":
+		// 	node.HandleGetBlockchain(msg)
+		default:
+			log.Printf("Unknown message type: %s\n", msg)
+		}
+	}
 }
