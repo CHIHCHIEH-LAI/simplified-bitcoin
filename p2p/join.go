f6c880b70af657e6e9b39c32b67577b91ddc48f6
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-15 00:19:31 +0800

    Add HandleJoinRequest method to process JOINREQ messages and update member list

--- p2p/join.go
+++ p2p/join.go
@@ -2,16 +2,15 @@ package p2p
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
 )
 
+// IntroduceSelfToGroup sends a JOINREQ message to the bootstrap node
 func (node *Node) IntroduceSelfToGroup(bootstrapNodeAddress string) error {
-	// Construct and serialize JOINREQ message
-	sender := node.Address
-	payload := node.Address
-	message := message.NewMessage("JOINREQ", sender, payload)
+	message := message.NewMessage("JOINREQ", node.Address, "")
 	messageData := message.Serialize()
 
 	// Send JOINREQ message to bootstrap node
@@ -21,4 +20,18 @@ func (node *Node) IntroduceSelfToGroup(bootstrapNodeAddress string) error {
 	}
 
 	return nil
+}
+
+// HandleJoinRequest processes a JOINREQ message
+func (node *Node) HandleJoinRequest(msg message.Message) {
+	// Add the sender to the member list
+	member := Member{
+		Address:   msg.Sender,
+		Heartbeat: 0,
+		Timestamp: time.Now().Unix(),
+	}
+	node.MemberList = append(node.MemberList, member)
+
+	// Send JOINREP message to the sender
+
 }
