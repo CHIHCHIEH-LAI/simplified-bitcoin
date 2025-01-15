c0a762756c16a23aaa85a6dbc86faf6554575c50
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-16 00:02:45 +0800

    Enhance membership management by adding heartbeat functionality and updating member list handling

--- p2p/membership.go
+++ p2p/membership.go
@@ -10,6 +10,9 @@ import (
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
 )
 
+const TIMENODEFAIL = 30 * 60
+const TIMENODEREMOVE = 4 * TIMENODEFAIL
+
 type Member struct {
 	Address   string
 	Heartbeat int64
@@ -25,6 +28,51 @@ func NewJOINREQMessage(sender string) message.Message {
 	}
 }
 
+// NewJOINRESPMessage creates a new JOINRESP message
+func NewJOINRESPMessage(sender string, payload string) message.Message {
+	return message.Message{
+		Type:    "JOINRESP",
+		Sender:  sender,
+		Payload: payload,
+	}
+}
+
+// NewHEARTBEATMessage creates a new HEARTBEAT message
+func NewHEARTBEATMessage(sender string, payload string) message.Message {
+	return message.Message{
+		Type:    "HEARTBEAT",
+		Sender:  sender,
+		Payload: payload,
+	}
+}
+
+// SerializeMemberList serializes a list of members into a string
+func SerializeMemberList(memberList []Member) string {
+	str := ""
+	for _, member := range memberList {
+		str += fmt.Sprintf("%s:%d:%d,", member.Address, member.Heartbeat, member.Timestamp)
+	}
+	return str
+}
+
+// DeserializeMemberList deserializes a string into a list of members
+func DeserializeMemberList(str string) ([]Member, error) {
+	memberList := []Member{}
+	members := strings.Split(str, ",")
+	for _, memberStr := range members {
+		if memberStr == "" {
+			continue
+		}
+		var member Member
+		_, err := fmt.Sscanf(memberStr, "%s:%d:%d", &member.Address, &member.Heartbeat, &member.Timestamp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to deserialize member: %v", err)
+		}
+		memberList = append(memberList, member)
+	}
+	return memberList, nil
+}
+
 // IntroduceSelfToGroup sends a JOINREQ message to the bootstrap node
 func (node *Node) IntroduceSelfToGroup(bootstrapNodeAddress string) error {
 	// Create a JOINREQ message and serialize it
@@ -38,24 +86,6 @@ func (node *Node) IntroduceSelfToGroup(bootstrapNodeAddress string) error {
 	}
 
 	return nil
-}
-
-// NewJOINRESPMessage creates a new JOINRESP message
-func NewJOINRESPMessage(sender string, payload string) message.Message {
-	return message.Message{
-		Type:    "JOINRESP",
-		Sender:  sender,
-		Payload: payload,
-	}
-}
-
-// SerializeMemberList serializes a list of members into a string
-func SerializeMemberList(memberList []Member) string {
-	str := ""
-	for _, member := range memberList {
-		str += fmt.Sprintf("%s:%d:%d,", member.Address, member.Heartbeat, member.Timestamp)
-	}
-	return str
 }
 
 // HandleJoinRequest processes a JOINREQ message
@@ -78,24 +108,6 @@ func (node *Node) HandleJoinRequest(msg message.Message) {
 	}
 }
 
-// DeserializeMemberList deserializes a string into a list of members
-func DeserializeMemberList(str string) ([]Member, error) {
-	memberList := []Member{}
-	members := strings.Split(str, ",")
-	for _, memberStr := range members {
-		if memberStr == "" {
-			continue
-		}
-		var member Member
-		_, err := fmt.Sscanf(memberStr, "%s:%d:%d", &member.Address, &member.Heartbeat, &member.Timestamp)
-		if err != nil {
-			return nil, fmt.Errorf("failed to deserialize member: %v", err)
-		}
-		memberList = append(memberList, member)
-	}
-	return memberList, nil
-}
-
 // HandleJoinResponse processes a JOINRESP message
 func (node *Node) HandleJoinResponse(msg message.Message) {
 	// Deserialize the member list from the payload
@@ -107,34 +119,6 @@ func (node *Node) HandleJoinResponse(msg message.Message) {
 
 	// Update the member list
 	node.MemberList = memberList
-}
-
-// NewHEARTBEATMessage creates a new HEARTBEAT message
-func NewHEARTBEATMessage(sender string, payload string) message.Message {
-	return message.Message{
-		Type:    "HEARTBEAT",
-		Sender:  sender,
-		Payload: payload,
-	}
-}
-
-// SendHeartbeat sends a heartbeat message to all members in the network
-func (node *Node) SendHeartbeat() {
-	// Create a HEARTBEAT message and serialize it
-	payload := SerializeMemberList(node.MemberList)
-	message := NewJOINRESPMessage(node.Address, payload)
-	messageData := message.Serialize()
-
-	// Send HEARTBEAT message to all members in the network
-	for _, member := range node.MemberList {
-		if member.Address == node.Address {
-			continue
-		}
-		err := network.SendMessageData(member.Address, messageData)
-		if err != nil {
-			log.Printf("Failed to send HEARTBEAT message: %v\n", err)
-		}
-	}
 }
 
 // HandleHeartbeat processes a HEARTBEAT message
@@ -152,4 +136,74 @@ func (node *Node) HandleHeartbeat(msg message.Message) {
 
 // UpdateMemberList updates the member list with the new list of members
 func (node *Node) UpdateMemberList(newMemberList []Member) {
+	for _, newMember := range newMemberList {
+		// Check if the member is self
+		if newMember.Address == node.Address {
+			continue
+		}
+
+		// Check if the member is already in the list
+		index := node.FindMemberInList(newMember.Address)
+		if index == -1 {
+			node.AddMemberToList(newMember)
+		} else {
+			node.UpdateMemberInList(index, newMember)
+		}
+
+	}
+}
+
+// FindMemberInList finds a member in the list by address
+func (node *Node) FindMemberInList(address string) int {
+	for i, member := range node.MemberList {
+		if member.Address == address {
+			return i
+		}
+	}
+	return -1
+}
+
+// AddMemberToList adds a member to the list
+func (node *Node) AddMemberToList(member Member) {
+	node.MemberList = append(node.MemberList, member)
+}
+
+// UpdateMemberInList updates a member in the list
+func (node *Node) UpdateMemberInList(index int, newMember Member) {
+	// Check if the newMember is failed
+	if time.Now().Unix()-newMember.Timestamp > TIMENODEFAIL {
+		return
+	}
+
+	// Update the member in the list
+	node.MemberList[index].Heartbeat = newMember.Heartbeat
+	node.MemberList[index].Timestamp = time.Now().Unix()
+}
+
+// MaintainMembership maintains the membership list by sending heartbeats
+func (node *Node) MaintainMembership() {
+	for {
+		node.SendHeartbeat()
+		time.Sleep(5 * time.Second)
+	}
+}
+
+// SendHeartbeat sends a heartbeat message to all members in the network
+func (node *Node) SendHeartbeat() {
+	// Create a HEARTBEAT message and serialize it
+	payload := SerializeMemberList(node.MemberList)
+	message := NewJOINRESPMessage(node.Address, payload)
+	messageData := message.Serialize()
+
+	// Send HEARTBEAT message to all members in the network
+	for _, member := range node.MemberList {
+		// Skip self
+		if member.Address == node.Address {
+			continue
+		}
+		err := network.SendMessageData(member.Address, messageData)
+		if err != nil {
+			log.Printf("Failed to send HEARTBEAT message: %v\n", err)
+		}
+	}
 }
