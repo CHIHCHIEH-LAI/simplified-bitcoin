ad830db144760eea4e9252af5fcc5b7f694a8286
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-17 23:51:36 +0800

    Add GenerateTransactionID method to Transaction struct for unique ID generation

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,5 +1,11 @@
 package transaction
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
 type Transaction struct {
 	TransactionID string
 	Sender        string
@@ -8,4 +14,10 @@ type Transaction struct {
 	Fee           float64
 	Timestamp     int64
 	Signature     string
+}
+
+func (tx *Transaction) GenerateTransactionID() string {
+	data := fmt.Sprintf("%s%s%f%f%d", tx.Sender, tx.Recipient, tx.Amount, tx.Fee, tx.Timestamp)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
 }
