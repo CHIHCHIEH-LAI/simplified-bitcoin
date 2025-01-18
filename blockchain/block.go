778c624acc769dc49882023a2de9d84834fd1700
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-18 19:16:37 +0800

    Add Block struct and methods for block creation and ID generation

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,1 +1,36 @@
 package blockchain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/transaction"
+)
+
+type Block struct {
+	BlockID      string
+	PrevHash     string
+	Timestamp    int64
+	Nonce        int // Nonce is a number that miners use to change the hash of the block
+	Transactions []*transaction.Transaction
+}
+
+// GenerateBlockID generates a unique ID for the block
+func (b *Block) GenerateBlockID() string {
+	data := fmt.Sprintf("%s%d%d", b.PrevHash, b.Timestamp, b.Nonce)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
+
+// NewBlock creates a new block with the given previous hash and transactions
+func NewBlock(prevHash string, transactions []*transaction.Transaction) *Block {
+	block := &Block{
+		PrevHash:     prevHash,
+		Timestamp:    0,
+		Nonce:        0,
+		Transactions: transactions,
+	}
+	block.BlockID = block.GenerateBlockID()
+	return block
+}
