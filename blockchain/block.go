e7a92e410fa3659f7f7edf18fbd1635354bc92b0
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-19 21:23:01 +0800

    Add validation methods for block ID and previous hash

--- blockchain/block.go
+++ blockchain/block.go
@@ -39,3 +39,21 @@ func NewBlock(prevHash string, transactions []*transaction.Transaction) *Block {
 func NewGenesisBlock() *Block {
 	return NewBlock("", nil)
 }
+
+// ValidateBlockID validates the block ID
+func (b *Block) ValidateBlockID() error {
+	if b.BlockID != b.GenerateBlockID() {
+		return fmt.Errorf("invalid block ID")
+	}
+
+	return nil
+}
+
+// ValidatePrevHash validates the previous hash
+func (b *Block) ValidatePrevHash(prevHash string) error {
+	if b.PrevHash != prevHash {
+		return fmt.Errorf("invalid previous hash")
+	}
+
+	return nil
+}
