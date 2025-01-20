a9228a2172ed7e6e747a6e81004b2f0d96c395db
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-20 23:14:35 +0800

    Add timestamp field to JOINREQ, JOINRESP, and HEARTBEAT message constructors

--- membership/membership.go
+++ membership/membership.go
@@ -40,27 +40,30 @@ func NewMembershipManager(address string) *MembershipManager {
 // NewJOINREQMessage creates a new JOINREQ message
 func NewJOINREQMessage(sender string) message.Message {
 	return message.Message{
-		Type:    message.JOINREQ,
-		Sender:  sender,
-		Payload: "",
+		Type:      message.JOINREQ,
+		Sender:    sender,
+		Payload:   "",
+		Timestamp: utils.GetCurrentTimeInUnix(),
 	}
 }
 
 // NewJOINRESPMessage creates a new JOINRESP message
 func NewJOINRESPMessage(sender string, payload string) message.Message {
 	return message.Message{
-		Type:    message.JOINRESP,
-		Sender:  sender,
-		Payload: payload,
+		Type:      message.JOINRESP,
+		Sender:    sender,
+		Payload:   payload,
+		Timestamp: utils.GetCurrentTimeInUnix(),
 	}
 }
 
 // NewHEARTBEATMessage creates a new HEARTBEAT message
 func NewHEARTBEATMessage(sender string, payload string) message.Message {
 	return message.Message{
-		Type:    message.HEARTBEAT,
-		Sender:  sender,
-		Payload: payload,
+		Type:      message.HEARTBEAT,
+		Sender:    sender,
+		Payload:   payload,
+		Timestamp: utils.GetCurrentTimeInUnix(),
 	}
 }
 
