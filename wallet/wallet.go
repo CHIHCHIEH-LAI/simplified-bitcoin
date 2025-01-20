d176584b540400756d3870077f2000c62d02905b
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-20 23:51:13 +0800

    refactor: move transaction creation and sending from wallet.go to wallet/transaction.go in wallet package

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -6,12 +6,6 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
-	"time"
-
-	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
-	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
-	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/transaction"
 )
 
 type Wallet struct {
@@ -41,51 +35,4 @@ func (w *Wallet) Sign(data string) (string, error) {
 	}
 	signature := append(r.Bytes(), s.Bytes()...)
 	return hex.EncodeToString(signature), nil
-}
-
-// CreateTransaction creates a new transaction
-func (w *Wallet) CreateTransaction(recipient string, amount float64, fee float64) (*transaction.Transaction, error) {
-	// Create the transaction
-	tx := transaction.Transaction{
-		Sender:    w.GetAddress(),
-		Recipient: recipient,
-		Amount:    amount,
-		Fee:       fee,
-		Timestamp: time.Now().Unix(),
-	}
-
-	// Generate the transaction ID
-	tx.TransactionID = tx.GenerateTransactionID()
-
-	// Sign the transaction
-	data := fmt.Sprintf("%s%s%f%f%d", tx.Sender, tx.Recipient, tx.Amount, tx.Fee, tx.Timestamp)
-	signature, err := w.Sign(data)
-	if err != nil {
-		return nil, err
-	}
-	tx.Signature = signature
-
-	return &tx, nil
-}
-
-// SendTransaction sends a transaction to a node in the network
-func SendTransaction(tx *transaction.Transaction, selfAddress string, nodeAddress string) error {
-	// Serialize the transaction
-	txData := tx.Serialize()
-
-	// Create and serialize the mwssage
-	message := message.Message{
-		Type:    message.NEWTRANSACTION,
-		Sender:  selfAddress,
-		Payload: txData,
-	}
-	messageData := message.Serialize()
-
-	// Send the message to the node
-	err := network.SendMessageData(nodeAddress, messageData)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
