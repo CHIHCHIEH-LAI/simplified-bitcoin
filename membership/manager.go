0e9b7f52ab8b0e4d28ebc03c910ba489e9d9b7d1
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-28 17:00:53 +0800

    refactor: move SelectNMembers method to MembershipManager and remove member_selector.go

--- membership/manager.go
+++ membership/manager.go
@@ -1,6 +1,10 @@
 package membership
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/exp/rand"
+)
 
 type MembershipManager struct {
 	Address    string
@@ -23,4 +27,34 @@ func (mgr *MembershipManager) MaintainMembership() {
 		mgr.SendHeartbeat()
 		time.Sleep(TIMEHEARTBEAT * time.Second)
 	}
+}
+
+// SelectMembers selects n_member random members from the member list
+func (mgr *MembershipManager) SelectNMembers(n_target int) []string {
+	selectedMembers := make(map[int]bool)
+	limit := min(n_target, len(mgr.MemberList.Members)-1)
+
+	for len(selectedMembers) < limit {
+		index := rand.Intn(len(mgr.MemberList.Members))
+
+		// Skip self
+		if mgr.MemberList.Members[index].Address == mgr.Address {
+			continue
+		}
+
+		// Skip if the member is already selected
+		if _, ok := selectedMembers[index]; ok {
+			continue
+		}
+
+		selectedMembers[index] = true
+	}
+
+	// Convert the selected members to a slice
+	selectedMembersSlice := make([]string, 0)
+	for index := range selectedMembers {
+		selectedMembersSlice = append(selectedMembersSlice, mgr.MemberList.Members[index].Address)
+	}
+
+	return selectedMembersSlice
 }
