7c75ae3ada0501c10b2176304b952bd161cdea6a
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-29 23:53:56 +0800

    refactor: streamline message handling in Node by utilizing Tranceiver for message reception and transmission

--- node/message.go
+++ node/message.go
@@ -1,25 +1,17 @@
 package node
 
 import (
-	"fmt"
 	"log"
 	"math"
 
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
-	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
 )
 
 // HandleIncomingMessage processes incoming messages
 func (node *Node) HandleIncomingMessage() {
-	for msgData := range node.MessageChannel {
-		log.Printf("Received message: %s\n", msgData)
-
-		// Parse the message
-		msg, err := message.DeserializeMessage(msgData)
-		if err != nil {
-			log.Printf("Failed to deserialize message: %v\n", err)
-			continue
-		}
+	for {
+		// Receive a message
+		msg := node.Tranceiver.Receive()
 
 		// Process the message based on its type
 		switch msg.Type {
@@ -44,10 +36,6 @@ func (node *Node) HandleIncomingMessage() {
 
 // GossipMessage sends a message to N random members
 func (node *Node) GossipMessage(msg *message.Message) error {
-	msgData, err := msg.Serialize()
-	if err != nil {
-		return fmt.Errorf("failed to serialize message: %v", err)
-	}
 
 	// Select N random members to send the message to
 	n_members := len(node.MembershipManager.MemberList.Members)
@@ -56,10 +44,8 @@ func (node *Node) GossipMessage(msg *message.Message) error {
 
 	// Send the message to the selected members
 	for _, member := range selectedMembers {
-		err := network.SendMessageData(member.Address, msgData)
-		if err != nil {
-			log.Printf("Failed to send message to %s: %v\n", member.Address, err)
-		}
+		msg.Receipient = member.Address
+		node.Tranceiver.Transmit(msg)
 	}
 
 	return nil
