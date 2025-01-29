959245d0a73046b623e0fdc5851220bc1f1b03e5
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-29 23:26:39 +0800

    refactor: replace listener functionality with Receiver for improved message handling

---
+++ network/receiver.go
@@ -0,0 +1,78 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
+)
+
+type Receiver struct {
+	Listener       net.Listener           // Listener to accept incoming connections
+	MessageChannel chan<- message.Message // Channel to send received messages
+}
+
+// NewReceiver creates a new Receiver instance
+func NewReceiver(port string, messageChannel chan<- message.Message) (*Receiver, error) {
+	// Start listening on the specified port
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start server on port %s: %v", port, err)
+	}
+
+	return &Receiver{
+		Listener:       listener,
+		MessageChannel: messageChannel,
+	}, nil
+}
+
+// Start starts the receiver
+func (r *Receiver) Run() {
+	for {
+		// Accept a single connection
+		conn, err := r.Listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %v\n", err)
+			conn.Close()
+			continue
+		}
+
+		// Handle the connection in a separate goroutine
+		r.handleConnection(conn)
+
+		// Close the connection
+		conn.Close()
+	}
+}
+
+// handleConnection handles incoming connections
+func (r *Receiver) handleConnection(conn net.Conn) {
+	// Read data from the connection
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Failed to read from connection: %v\n", err)
+		return
+	}
+
+	// Deserialize the received message data
+	data := string(buffer[:n])
+	msg, err := message.DeserializeMessage(data)
+	if err != nil {
+		log.Printf("Failed to deserialize message: %v\n", err)
+		return
+	}
+
+	// Send the message to the message channel
+	r.MessageChannel <- *msg
+}
+
+// Close closes the receiver
+func (r *Receiver) Close() {
+	// Close the listener
+	r.Listener.Close()
+
+	// Close the message channel
+	close(r.MessageChannel)
+}
