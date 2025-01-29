4805e0daf4292d31a72c18400c741b0c0dc77c78
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-29 19:06:43 +0800

    refactor: rename message_handler.go to message.go and add BroadcastMessage function

--- node/message.go
+++ node/message.go
@@ -1,9 +1,12 @@
 package node
 
 import (
+	"fmt"
 	"log"
+	"math"
 
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/network"
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/transaction"
 )
 
@@ -38,4 +41,27 @@ func (node *Node) HandleMessage() {
 			log.Printf("Unknown message type: %s\n", msg.Type)
 		}
 	}
+}
+
+// BroadcastMessage broadcasts a message to N random members
+func (node *Node) BroadcastMessage(msg *message.Message) error {
+	msgData, err := msg.Serialize()
+	if err != nil {
+		return fmt.Errorf("failed to serialize message: %v", err)
+	}
+
+	// Select N random members to send the message to
+	n_members := len(node.MembershipManager.MemberList.Members)
+	n_targetMember := int(math.Sqrt(float64(n_members)))
+	selectedMembers := node.MembershipManager.SelectNMembers(n_targetMember)
+
+	// Send the message to the selected members
+	for _, member := range selectedMembers {
+		err := network.SendMessageData(member.Address, msgData)
+		if err != nil {
+			log.Printf("Failed to send message to %s: %v\n", member.Address, err)
+		}
+	}
+
+	return nil
 }
