98c9647ef0cdef4a2d034684ce732aedea6d8bc0
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-30 10:23:47 +0800

    refactor: rename mempool/message.go to transaction/message.go

--- transaction/message.go
+++ transaction/message.go
@@ -1,13 +1,12 @@
-package mempool
+package transaction
 
 import (
 	"fmt"
 
 	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/message"
-	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/transaction"
 )
 
-func NewMessage(sender, receipient string, tx *transaction.Transaction) (*message.Message, error) {
+func NewMessage(sender, receipient string, tx *Transaction) (*message.Message, error) {
 	txData, err := tx.Serialize()
 	if err != nil {
 		return nil, fmt.Errorf("failed to serialize transaction: %v", err)
