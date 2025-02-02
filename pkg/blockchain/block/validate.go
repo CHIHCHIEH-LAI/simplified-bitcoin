4c1b029edeecb502f5bb6f948599b055515bd513
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-02-02 22:05:20 +0800

    refactor: consolidate block validation methods

--- pkg/blockchain/block/validate.go
+++ pkg/blockchain/block/validate.go
@@ -2,19 +2,20 @@ package block
 
 import "fmt"
 
-// ValidateBlockID validates the block ID
-func (b *Block) ValidateBlockID() error {
-	if b.BlockID != b.GenerateBlockID() {
-		return fmt.Errorf("invalid block ID")
+// Validate validates the block
+func (b *Block) Validate() error {
+	// Validate the block ID
+	if err := b.validateBlockID(); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-// ValidatePrevHash validates the previous hash
-func (b *Block) ValidatePrevHash(prevHash string) error {
-	if b.PrevHash != prevHash {
-		return fmt.Errorf("invalid previous hash")
+// ValidateBlockID validates the block ID
+func (b *Block) validateBlockID() error {
+	if b.BlockID != b.GenerateBlockID() {
+		return fmt.Errorf("invalid block ID")
 	}
 
 	return nil
