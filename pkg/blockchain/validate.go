45bb08c9b65bbec31e7d7e88966f3ad4e6d7128b
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-02-02 22:04:41 +0800

    refactor: improve block validation and mutex handling

--- pkg/blockchain/validate.go
+++ pkg/blockchain/validate.go
@@ -1,22 +1,31 @@
 package blockchain
 
-import "github.com/CHIHCHIEH-LAI/simplified-bitcoin/pkg/blockchain/block"
+import (
+	"fmt"
+
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/pkg/blockchain/block"
+)
 
 // ValidateBlock validates the block
 func (bc *Blockchain) ValidateBlock(b *block.Block) error {
-	bc.mutex.Lock()
-	defer bc.mutex.Unlock()
-
-	// Validate the block ID
-	if err := b.ValidateBlockID(); err != nil {
-		return err
-	}
-
 	// Validate the previous hash
-	latestBlock := bc.GetLatestBlock()
-	if err := b.ValidatePrevHash(latestBlock.BlockID); err != nil {
+	if err := bc.validatePrevHash(b); err != nil {
 		return err
 	}
 
+	// Validate the block
+	if err := b.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// validatePrevHash validates the previous hash
+func (bc *Blockchain) validatePrevHash(b *block.Block) error {
+	latestBlock := bc.GetLatestBlock()
+	if b.PrevHash != latestBlock.BlockID {
+		return fmt.Errorf("invalid previous hash: %s", b.PrevHash)
+	}
 	return nil
 }
