7538f63521bd13d491d87848b2d338d3d4e8f5ab
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-02-04 21:43:03 +0800

    refactor: improve signature verification error handling in VerifySignature function

--- pkg/utils/cryto.go
+++ pkg/utils/cryto.go
@@ -5,18 +5,38 @@ import (
 	"crypto/elliptic"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"math/big"
 )
 
 // VerifySignature checks if the given signature is valid for the given data
-func VerifySignature(publicKey, data, signature string) bool {
-	pubKeyBytes, _ := hex.DecodeString(publicKey)
+func VerifySignature(publicKey, data, signature string) error {
+	pubKeyBytes, err := hex.DecodeString(publicKey)
+	if err != nil || len(pubKeyBytes) < 64 {
+		return fmt.Errorf("invalid public key")
+	}
+
+	signatureBytes, err := hex.DecodeString(signature)
+	if err != nil || len(signatureBytes) < 64 {
+		return fmt.Errorf("invalid signature")
+	}
+
+	// Extract X and Y coordinates for public key
 	x := new(big.Int).SetBytes(pubKeyBytes[:32])
 	y := new(big.Int).SetBytes(pubKeyBytes[32:])
-	pubKey := ecdsa.PublicKey{Curve: elliptic.P256(), X: x, Y: y}
-	signatureBytes, _ := hex.DecodeString(signature)
+
+	// Extract R and S values for the signature
 	r := new(big.Int).SetBytes(signatureBytes[:32])
 	s := new(big.Int).SetBytes(signatureBytes[32:])
+
+	// Hash the message
 	hash := sha256.Sum256([]byte(data))
-	return ecdsa.Verify(&pubKey, hash[:], r, s)
+
+	// Verify the signature
+	pubKey := ecdsa.PublicKey{Curve: elliptic.P256(), X: x, Y: y}
+	if ecdsa.Verify(&pubKey, hash[:], r, s) {
+		return nil
+	} else {
+		return fmt.Errorf("invalid signature")
+	}
 }
