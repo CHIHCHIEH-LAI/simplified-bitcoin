bf7729ac1a97cc0709bd8ef8d36f3329d60369f1
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-02-10 13:41:44 +0800

    refactor: implement message handling for incoming messages and blockchain synchronization

---
+++ pkg/node/message_handle.go
@@ -0,0 +1,73 @@
+package node
+
+import (
+	"log"
+
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/pkg/blockchain"
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/pkg/blockchain/block"
+	"github.com/CHIHCHIEH-LAI/simplified-bitcoin/pkg/message"
+)
+
+// HandleIncomingMessage processes incoming messages
+func (node *Node) handleIncomingMessage() {
+	for {
+		msg, ok := node.Transceiver.Receive()
+		if !ok {
+			continue // Skip iteration if no message
+		}
+
+		// Process the message based on its type
+		switch msg.Type {
+		case message.JOINREQ:
+			node.MembershipManager.HandleJoinRequest(msg)
+		case message.JOINRESP:
+			node.MembershipManager.HandleJoinResponse(msg)
+		case message.HEARTBEAT:
+			node.MembershipManager.HandleHeartbeat(msg)
+		case message.NEWTRANSACTION:
+			node.GossipManager.Gossip(msg)
+			node.Mempool.HandleNewTransaction(msg)
+		case message.NEWBLOCK:
+			node.handleNewBlockMsg(msg)
+		case message.BLOCKCHAINREQ:
+			node.handleBlockChainRequest(msg)
+		case message.BLOCKCHAINRESP:
+			node.handleBlockchainResponse(msg)
+		default:
+			log.Printf("Unknown message type: %s\n", msg.Type)
+		}
+	}
+}
+
+// handleNewBlockMsg handles a new block message
+func (node *Node) handleNewBlockMsg(msg *message.Message) {
+	block, _ := block.DeserializeBlock(msg.Payload)
+	if err := node.Blockchain.ValidateBlock(block); err != nil {
+		log.Printf("Invalid block: %s\n", err)
+		msg := message.NewMessage(message.BLOCKCHAINREQ, node.IPAddress, msg.Sender, "")
+		node.Transceiver.Transmit(msg)
+	} else {
+		node.Miner.Stop()
+		node.Blockchain.AddBlock(block)
+		go node.Miner.Run()
+	}
+}
+
+// handleBlockChainRequest handles a blockchain request message
+func (node *Node) handleBlockChainRequest(msg *message.Message) {
+	payload, _ := node.Blockchain.Serialize()
+	newMsg := message.NewMessage(message.BLOCKCHAINRESP, node.IPAddress, msg.Sender, payload)
+	node.Transceiver.Transmit(newMsg)
+}
+
+// handleBlockchainResponse handles a blockchain response message
+func (node *Node) handleBlockchainResponse(msg *message.Message) {
+	blockchain, _ := blockchain.DeserializeBlockchain(msg.Payload)
+	if err := node.Blockchain.ShouldSwitchChain(blockchain); err != nil {
+		log.Printf("Invalid blockchain: %s\n", err)
+	} else {
+		node.Miner.Stop()
+		node.Blockchain.SwitchChain(blockchain)
+		go node.Miner.Run()
+	}
+}
